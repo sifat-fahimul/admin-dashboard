a03263740ea257724ec6339f6676c70182689f6d
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-01-25 16:59:10 +0600

    error 404 page done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h1>404 page not found!</h1>
+    <div className="bg-gray-100 h-screen m-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className="">
+          <img
+            className="w-fit"
+            src="https://wp.alithemes.com/html/evara/evara-backend/assets/imgs/theme/404.png"
+            alt=""
+          />
+          <div className="text-center">
+            <p className="text-2xl my-5"> Oops! Page not found</p>
+            <Link
+              to="/"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Go Home
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
